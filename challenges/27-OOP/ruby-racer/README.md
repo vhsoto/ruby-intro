2e497268257f0bf67be343ac1265f29510e337b2
juan267 <j.gomez267@gmail.com>
2017-02-23 18:51:16 -0500

    fix ruby racer

--- challenges/27-OOP/ruby-racer/README.md
+++ challenges/27-OOP/ruby-racer/README.md
@@ -1,36 +1,13 @@
-# Ruby Racer 1 Outrageous Fortune
+# Ruby Racer
 
-##Learning Competencies
+##Resumen
 
-* Work with input / output from the console
-* Model a simple real-world system in Ruby code
+Vamos a construir un juego que simule una carrera de piques. Existen dos jugadores. Cada uno empieza en un "carro" al principio de la pista, lado a lado. La pista tiene una logitud de `n` cuadrados. Los jugadores toman turnos lanzando un dado para avanzar su carro de acuerdo al valor del dado.
 
-##Summary
+Este reto es sobre modelar un juego con muy poca interacion humana y sin estratagia. Te va tocar aprender un poco sobre como funciona imprimir en la consola, dado que tienes que imprimir el tablero. Te damos un codigo inicial el cual esta bien escrito, aprende de su estilo, los metodos que existen sus inputs y los valores que retornan, y como estos segmentan las acciones necesarias para correr el juego.
 
-Vroom vroom! We're going to build a simple game called *Ruby Racer*. This is a "game" like [Chutes and Ladders](http://en.wikipedia.org/wiki/Snakes_and_Ladders) is a game: the fixed board layout and random rolls of the dice completely determine the outcome.
+##Pasos
 
-The game works like a (random and much quieter) drag race. There are two players. Each starts with a "car" sitting at the beginning of the track, side-by-side. The track is `N` squares long. Players take turns rolling a die and advance their car according to the value of the roll.
+###Paso 0 : Solucion usando el codigo inicial
 
-We'll create less fatalistic versions in future challenges.
-
-This challenge is about modeling a simple game with little human interaction and no strategy. You'll learn a bit about how console output works when you have to print the board. The starting code is well-written, so pay attention to the style, what methods exist, what their inputs and return values are, and how they segment the actions necessary to "play" the game.
-
-##Releases
-
-###Release 0 : Implement Using the Skeleton Code
-
-The source gist for this challenge contains a lot of skeleton code; use it! You should mostly be filling out the pre-defined `RubyRacer` class, although feel free to dress up the output and have a little fun with the logic of the game. Maybe add some totally sweet [ASCII art](https://github.com/miketierney/artii)?
-
-Here's what the output should look like, roughly:  [link to video](https://www.youtube.com/embed/a4u4hV7Cguw?hd=1&amp;showinfo=0).
-
-**Note**: Are you assuming there are only two players and they're always labeled `'a'` and `'b'`? Why? If it's intentional as a means to get a simpler version up and running, that's fine, but it's important to be clear about what assumptions your program is making and why! Try to write a version that accepts any number of players (identified by single characters) as input.
-
-###Release 1 : Think of Fun Additions
-
-As a thought experiment, think about what would make this game more fun? Powerups? Landmines that send you back to the start? More player control?
-
-Is your code well-suited to adding these features or would it be difficult?
-
-<!-- ##Optimize Your Learning -->
-
-##Resources
+Este reto viene con bastante codigo inicial usalo!. Claramente tendras que rellenar la clase predefininda `RubyRacer`.
--- challenges/27-OOP/ruby-racer/source/ruby_racer.rb
+++ challenges/27-OOP/ruby-racer/source/ruby_racer.rb
@@ -1,162 +1,54 @@
-# require_relative 'racer_utils'
-
-# class Player
-#   attr_accessor :position, :name, :track
-
-#   def initialize(name, number, length)
-#     @position = 0
-#     @name = name
-#     @number = number
-#     @track = Array.new(length, "|")
-#   end
-# end
-
-# class RubyRacer
-#   attr_reader :players, :length
-
-#   def initialize(players, length = 30)
-#     @players = players
-#     @length = length
-#     @player_1 = Player.new(players[0], 1, @length)
-#     @player_2 = Player.new(players[1], 2, @length)
-#     @players = [@player_1, @player_2]
-#     @speed = Die.new
-#   end
-
-#   # Returns +true+ if one of the players has reached
-#   # the finish line, +false+ otherwise
-#   def finished?
-#     if @player_1.position >= 30 || @player_2.position >= 30
-#       true
-#     else
-#       false
-#     end
-#   end
-
-#   # Returns the winner if there is one, +nil+ otherwise
-#   def winner?
-#     if finished?
-#       champion = @player_1.position <=> @player_2.position
-#       if champion == 1
-#         return @player_1.name
-#       elsif champion == -1
-#         return @player_2.name
-#       else
-#         return "It's a tie!"
-#       end
-#     else
-#       nil
-#     end
-#   end
-
-#   # Rolls the dice and advances +player+ accordingly
-#   def advance_player!(player)
-#     player.track[player.position] = "|"
-#     player.position += @speed.roll
-#     player.track[player.position] = player.name
-#     move!
-#   end
-
-#   def move!
-#     clear_screen!
-#     move_to_home!
-#     puts @player_1.track.join(" ")
-#     puts @player_2.track.join(" ")
-#   end
-
-#   def print_board
-#     clear_screen!
-#     move_to_home!
-#     puts @player_1.track.insert(@player_1.position, @player_1.name).join(" ")
-#     puts @player_2.track.insert(@player_2.position, @player_2.name).join(" ")
-#   end
-
-#   def run
-#     start_player = @players.sample
-#     current_player = nil
-#     champion = winner?
-#     until champion
-#       advance_player!(start_player || current_player)
-#       current_player = @players.reject {|player| player == start_player || player == current_player}.first
-#       start_player = nil
-#       champion = winner?
-#       sleep 0.1
-#     end
-#     p champion
-#   end
-
-# end
-
-# game = RubyRacer.new(['juan', 'carolina'])
-# game.run
-
 require_relative 'racer_utils'
 
-class Player
-  attr_accessor :position, :name
-
-  def initialize(name)
-    @position = 0
-    @name = name
-  end
-end
-
-class RubyRacer < Player
+class RubyRacer
   attr_reader :players, :length
 
   def initialize(players, length = 30)
-    @players = players
-    @length = length
-    @player_1 = Player.new(players[0])
-    @player_2 = Player.new(players[1])
-    @players = [@player_1, @player_2]
   end
 
-  # Returns +true+ if one of the players has reached
-  # the finish line, +false+ otherwise
+  # Devuelve true si uno de los jugadores llego a la meta, falso de lo contrario
   def finished?
-    if @player_1.position >= 30 || @player_2.position >= 30
-      true
-    else
-      false
-    end
   end
 
-  # Returns the winner if there is one, +nil+ otherwise
+  # Retorna el ganador si hay uno, nil de lo contrario
   def winner
-    if finished?
-      champion = @player_1.position <=> @player_2.position
-      if champion == 1
-        return @player_1.name
-      elsif champion == -1
-        return @player_2.name
-      else
-        return "It's a tie!"
-      end
-    else
-      nil
-    end
   end
 
-  # Rolls the dice and advances +player+ accordingly
+  # Rueda el dado y avanza la posicion del jugador respectivo
   def advance_player!(player)
-    die = Die.new
-    @players.each do |racer|
-      if racer.name == player
-        racer.position += die.roll
-      end
-    end
   end
 
-  # Prints the current game board
-  # The board should have the same dimensions each time
-  # and you should print over the previous board
+  # Imprime el tablero actual
+  # El tablero siempre debe tener las mismas dimensiones
+  # Debes imprimir encima del tablero anterior
   def print_board
-    clear_screen!
-    move_to_home!
-    track_1 = Array.new(@length, "|")
-    track_2 = Array.new(@length, "|")
-    puts track_1.insert(@player_1.position, @player_1.name).join(" ")
-    puts track_2.insert(@player_2.position, @player_2.name).join(" ")
   end
 end
+
+#=========== driver code ===========
+
+players = ["A", "Z"]
+game = RubyRacer.new(players)
+
+# limpia la pantalla
+clear_screen!
+
+until game.finished?
+  players.each do |player|
+    # Mueve el cursor a la esquina derecha
+    move_to_home!
+
+    # imprimimos el tablero en su estado inicial
+    game.print_board
+    game.advance_player!(player)
+
+    # tenemos que dormir un poquito, de lo contrario no vamos a ver nada del juego.
+    # mira http://www.ruby-doc.org/core-1.9.3/Kernel.html#method-i-sleep
+    sleep(0.5)
+  end
+end
+
+# El juego termino
+game.print_board
+
+puts "El jugador'#{game.winner}' ha ganado!"#
