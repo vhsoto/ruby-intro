b30d90c11448deab2b3e62458243cbf6aa4fceea
juan267 <j.gomez267@gmail.com>
2017-07-27 19:12:13 -0500

    add tareas solution

---
+++ leerEscribirTareas.rb
@@ -0,0 +1,41 @@
+require 'pry'
+# load task:
+  # input: file
+    # 1,Hacer tareas,true
+    # 2,Lavar ropa,false
+  # Output:
+    # [
+    #   { id: 1, name: "Hacer tareas", done: true },
+    #   { id: 2, name: "Lavar ropa", done: false }
+    # ]
+
+def load_task(file)
+  tareas = []
+  file = File.open(file, 'r')
+  File.open(file, 'r') do |file|
+    file.each do |line|
+      line = line.split(',').map(&:strip)
+      tareas << {id: line[0].to_i, name: line[1], done: line[2] == 'true'}
+    end
+  end
+  tareas
+end
+
+def save_task(file, tareas)
+  File.open(file, 'w+') do |file|
+    tareas.each do |tarea|
+      line = []
+      tarea.each do |key, value|
+        line << value
+      end
+      file.puts line.join(',')
+    end
+  end
+end
+
+
+load_task('tareas.txt')
+save_task('tareas.txt', [
+    { id: 1, name: "Hacer tareas", done: true },
+    { id: 2, name: "Lavar ropa", done: false }
+])
