7f15f200933c37e8d712d5e09fa2b6bb0e2756da
juan267 <j.gomez267@gmail.com>
2017-10-12 19:38:42 -0500

    Add ruby types notes for onix

---
+++ notes/onix/hola_mundo.rb
@@ -0,0 +1,44 @@
+# Tipos de datos
+
+# Primitivos
+Booleano
+true false
+Integer
+1 -1 -3 0
+Float
+1.0 -1.3
+Symbol
+
+# Estructura de datos
+String
+"hola" 'hola'
+
+"don't"
+
+Array
+
+lista = [1, 2, 'hola', false, 4.4, [['chao', 3, 5]]]
+lista[0]
+lista[0] = 'hola'
+
+
+Hash
+pepito = ['pepito', 'hernandez', 28, 12.324]
+pepito[1]
+
+# Syntactic Sugar
+pepito = {
+  "nombre" => 'pepito',
+  "apellido" => 'hernandez',
+  age: 28,
+  height: 12.324,
+  alive: true,
+  childrens: ['maria', 'josefina']
+}
+
+pepito["apellido"]
+
+
+Range
+0..-1
+
