1b69b2623652f17d1ccc36321b70ecaf7ca3f1d8
vhsoto <vhsotoing@gmail.com>
2017-11-02 18:56:12 -0500

    Ejercicio 5 resuelto, pruebas OK

--- challenges/active-record/5-validations/source/app/models/person.rb
+++ challenges/active-record/5-validations/source/app/models/person.rb
@@ -1,2 +1,5 @@
 class Person < ActiveRecord::Base
+	validates :first_name, :last_name, presence: true
+	validates :first_name, :last_name, length: { minimum: 2 }
+	validates :first_name, :last_name, uniqueness: true
 end
--- challenges/active-record/5-validations/source/app/models/rating.rb
+++ challenges/active-record/5-validations/source/app/models/rating.rb
@@ -1,2 +1,5 @@
 class Rating < ActiveRecord::Base
+	validates :coolness, :cuteness, :judge_id, :dog_id, presence: true
+	validates :coolness, :cuteness, :judge_id, :dog_id, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 10 }
+	validates :judge_id, uniqueness: true
 end
